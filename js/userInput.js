44d2e379b6cca6fa9222be27f878f38b0d8b4c53
NancyNancy07 <148770109+NancyNancy07@users.noreply.github.com>
2024-04-20 17:33:19 +0200

    remove unused css

--- js/userInput.js
+++ js/userInput.js
@@ -1,16 +1,18 @@
-// user input
-const headingElement = document.querySelector("#heading");
-const subHeadingElement = document.querySelector("#sub-heading");
-const inputElement = document.querySelector("#input-name");
-const buttonElement = document.querySelector(".button");
-buttonElement.addEventListener("click", function (event) {
-  event.preventDefault();
-  let enteredName = inputElement.value;
-  if (enteredName !== "") {
-    headingElement.innerText = "Thank You!" + " " + enteredName;
-    subHeadingElement.innerText = "For Contacting Me...";
-  } else {
-    headingElement.innerText = "Contact Me";
-    subHeadingElement.innerText = " Fill this Form to Contact";
-  }
-});
+
+function sendMessage() {
+  var name = document.getElementById("input-name").value;
+  var email = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("messege").value;
+
+  var mailtoLink =
+    "mailto:13nancyrajput25@gmail.com" +
+    "?subject=" +
+    encodeURIComponent(subject) +
+    "&body=" +
+    encodeURIComponent(
+      "Name: " + name + "\nEmail: " + email + "\n\n" + message
+    );
+
+  window.location.href = mailtoLink;
+}
